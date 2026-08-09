3b246a560003b0d58afade3b5f4cb47540bbaf26
muhammadnoumandurrani892-jpg <muhammadnoumandurrani892@gmail.com>
2026-08-09 16:14:08 +0500

    Configure Android application settings in build.gradle.kts
    Updated Android Gradle plugin and Kotlin versions, added Android configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,34 @@
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.dsmart.security"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.dsmart.security"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.media3:media3-exoplayer:1.2.1")
+    implementation("androidx.media3:media3-exoplayer-rtsp:1.2.1")
+    implementation("androidx.media3:media3-ui:1.2.1")
 }
