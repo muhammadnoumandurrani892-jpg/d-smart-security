c4995e1871b8e4c8c5f6211b24c30c9ab1d0b7ed
muhammadnoumandurrani892-jpg <muhammadnoumandurrani892@gmail.com>
2026-08-09 17:54:19 +0500

    Refactor settings.gradle.kts for project structure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,23 +6,6 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "d-smart-security"
-include(":app")
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
