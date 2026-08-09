d9b21ca3564fd918ba3d2549f7309baece6bf4e7
muhammadnoumandurrani892-jpg <muhammadnoumandurrani892@gmail.com>
2026-08-09 17:45:47 +0500

    Configure plugin and dependency management in settings
    Added plugin management and dependency resolution management sections with repository configurations.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,3 +16,23 @@ dependencyResolutionManagement {
 
 rootProject.name = "d-smart-security"
 include(":app")
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "D Smart"
+
+include(":app")
